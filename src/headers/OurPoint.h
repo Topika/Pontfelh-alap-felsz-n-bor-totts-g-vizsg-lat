59be0473a4bb0ec8048acc94453e0aba67f0d84e
tothgabi <kistoth@inf.elte.hu>
2018-04-05 11:04:04 +0200

    make same 'tab' indent

--- src/headers/OurPoint.h
+++ src/headers/OurPoint.h
@@ -5,48 +5,47 @@
 #include <cmath>
 
 enum preProcClass {
-  undef,
-  upperContour, lowerContour,
-  uniformSurface, nonUniformSurface
+	undef,
+	upperContour, lowerContour,
+	uniformSurface, nonUniformSurface
 };
 
 class OurPoint {
-  
-  //private datas
-  private:
-    double x;
-    double y;
-    double z;
-	int returns;
-    preProcClass preClass;
 
-  //public methods
-  public:
-    OurPoint (double x_, double y_, double z_, int returns_) : 
-      x(x_), y(y_), z(z_), preClass(undef), returns(returns_) {}
+	//private datas
+	private:
+		double x;
+		double y;
+		double z;
+			int returns;
+		preProcClass preClass;
 
-    ~OurPoint() {}
+	//public methods
+	public:
+	OurPoint (double x_, double y_, double z_, int returns_) : 
+		x(x_), y(y_), z(z_), preClass(undef), returns(returns_) {}
 
-    //getters
-    double getX() const { return x; }
-    double getY() const { return y; }
-    double getZ() const { return z; }
-	int getReturns() const { return returns; }
-    preProcClass getPreClass() const { return preClass; }
+	~OurPoint() {}
 
-    //setters
-    void setX(double x1) { x = x1; }
-    void setY(double y1) { y = y1; }
-    void setZ(double z1) { z = z1; }
-	void setReturns(int ret) { returns = ret; }
-    void setPreClass(preProcClass p1) { preClass = p1; }
+	//getters
+	double getX() const { return x; }
+	double getY() const { return y; }
+	double getZ() const { return z; }
+		int getReturns() const { return returns; }
+	preProcClass getPreClass() const { return preClass; }
 
-    //other methods
-    double distanceFromInZ(const OurPoint& p2) const {
-      return (p2.z - z);
-    }
+	//setters
+	void setX(double x1) { x = x1; }
+	void setY(double y1) { y = y1; }
+	void setZ(double z1) { z = z1; }
+		void setReturns(int ret) { returns = ret; }
+	void setPreClass(preProcClass p1) { preClass = p1; }
+
+	//other methods
+	double distanceFromInZ(const OurPoint& p2) const {
+		return (p2.z - z);
+	}
 };
 
 #endif //OURPOINT_H
 
-    
--- src/test.cc
+++ src/test.cc
@@ -13,20 +13,20 @@ using namespace boost::polygon;
 namespace boost {
 namespace polygon {
 
-  template <>
-  struct geometry_concept<OurPoint> {
-    typedef point_concept type;
-  };
+	template <>
+	struct geometry_concept<OurPoint> {
+		typedef point_concept type;
+	};
 
-  template <>
-  struct point_traits<OurPoint> {
-    typedef int coordinate_type;
+	template <>
+	struct point_traits<OurPoint> {
+		typedef int coordinate_type;
 
-    static inline coordinate_type get(
-        const OurPoint& point, orientation_2d orient) {
-      return (orient == HORIZONTAL) ? point.getX() : point.getY();
-    }
-  };
+		static inline coordinate_type get(
+			const OurPoint& point, orientation_2d orient) {
+			return (orient == HORIZONTAL) ? point.getX() : point.getY();
+		}
+	};
 }  // polygon
 }  // boost
 
@@ -37,86 +37,83 @@ const double s1 = 2000.0; //?
 -- of the point with the threshold s1 
 */
 preProcClass classCalculate(const OurPoint &p, const vector<OurPoint> &neighbours) {
-  preProcClass resultClass = undef;
+	preProcClass resultClass = undef;
     
-  bool isInside, isUpper, isLower = false; 
-  for (auto nPoint : neighbours)
-  {
-    double distance = nPoint.distanceFromInZ(p);
-    if (distance < (-1 * s1))
-       isLower = true;
-    else if (distance > s1)
-      isUpper = true;
-    else
-      isInside = true;
-  }
+	bool isInside, isUpper, isLower = false; 
+	for (auto nPoint : neighbours)
+	{
+		double distance = nPoint.distanceFromInZ(p);
+		if (distance < (-1 * s1))
+			isLower = true;
+		else if (distance > s1)
+			isUpper = true;
+		else
+			isInside = true;
+	}
 
-  if (isInside && !isLower && !isUpper)
-    resultClass = uniformSurface; // it can be nonuniformSurface too, TODO
-  else if (isInside && isLower && !isUpper)
-    resultClass = lowerContour;
-  else if (isInside && !isLower && isUpper)
-     resultClass = upperContour;
+	if (isInside && !isLower && !isUpper)
+		resultClass = uniformSurface; // it can be nonuniformSurface too, TODO
+	else if (isInside && isLower && !isUpper)
+		resultClass = lowerContour;
+	else if (isInside && !isLower && isUpper)
+		resultClass = upperContour;
 
-  return resultClass;
+	return resultClass;
 }
 
 vector<OurPoint> getNeighboursOfPoint(const voronoi_diagram<double>::cell_type &point, const std::vector<OurPoint> &inputPoints) {
-  vector<OurPoint> result;
-  if (point.contains_point() && !point.is_degenerate())
-  {
-    const voronoi_diagram<double>::edge_type *edge = point.incident_edge();
-    do
-    {
-      if (edge->is_primary())
-      {
-        auto index = edge->twin()->cell()->source_index();
-        result.push_back(inputPoints[index]);
-      }
-      edge = edge->next();
-    } while (edge != point.incident_edge());
-  }
-  return result;
+	vector<OurPoint> result;
+	if (point.contains_point() && !point.is_degenerate())
+	{
+		const voronoi_diagram<double>::edge_type *edge = point.incident_edge();
+		do
+		{
+			if (edge->is_primary())
+			{
+				auto index = edge->twin()->cell()->source_index();
+				result.push_back(inputPoints[index]);
+			}
+			edge = edge->next();
+		} while (edge != point.incident_edge());
+	}
+	return result;
 }
-
 
 void iterateCells(const voronoi_diagram<double> &vd, std::vector<OurPoint> &inputPoints) {
-  std::ofstream ofs;
-  ofs.open("file.txt", std::ofstream::out | std::ofstream::app);
-  int upperCnt = 0, lowerCnt = 0, surfaceCnt = 0, undefCnt = 0;
-  
-  for (auto it = vd.cells().begin(); it != vd.cells().end(); ++it)
-  {
-    const voronoi_diagram<double>::cell_type &cell = *it;
-    //ofs << it->source_index() << std::endl;
-  
-    OurPoint& currentPoint = inputPoints[cell.source_index()];
-    preProcClass actualPointClass = classCalculate(currentPoint, getNeighboursOfPoint(cell, inputPoints));
-    currentPoint.setPreClass(actualPointClass);
-    
-    switch (currentPoint.getPreClass())
-    {
-    case upperContour: ++upperCnt; break;
-    case lowerContour: ++lowerCnt; break;
-    case uniformSurface:
-    case nonUniformSurface: ++surfaceCnt; break;
-    default: ++undefCnt; break;
-    }
-  }
-  ofs << "Upper: " << upperCnt << std::endl
-	  << "Lower: " << lowerCnt << std::endl
-	  << "Surface: " << surfaceCnt << std::endl
-	  << "Other: " << undefCnt << std::endl;
-  ofs.close();
-}
+	std::ofstream ofs;
+	ofs.open("file.txt", std::ofstream::out | std::ofstream::app);
+	int upperCnt = 0, lowerCnt = 0, surfaceCnt = 0, undefCnt = 0;
 
+	for (auto it = vd.cells().begin(); it != vd.cells().end(); ++it)
+	{
+		const voronoi_diagram<double>::cell_type &cell = *it;
+		//ofs << it->source_index() << std::endl;
+
+		OurPoint& currentPoint = inputPoints[cell.source_index()];
+		preProcClass actualPointClass = classCalculate(currentPoint, getNeighboursOfPoint(cell, inputPoints));
+		currentPoint.setPreClass(actualPointClass);
+
+		switch (currentPoint.getPreClass())
+		{
+			case upperContour: ++upperCnt; break;
+			case lowerContour: ++lowerCnt; break;
+			case uniformSurface:
+			case nonUniformSurface: ++surfaceCnt; break;
+			default: ++undefCnt; break;
+		}
+	}
+	ofs << "Upper: " << upperCnt << std::endl
+		<< "Lower: " << lowerCnt << std::endl
+		<< "Surface: " << surfaceCnt << std::endl
+		<< "Other: " << undefCnt << std::endl;
+	ofs.close();
+}
 
 int main(int argc, char* argv[]) {
 	std::ifstream ifs;
-    
-    std::vector<OurPoint> points;    
-    
-    cout<<__LINE__<<std::endl;
+
+	std::vector<OurPoint> points;    
+
 	ifs.open("../data/sample1.las", std::ios::in | std::ios::binary);
 	liblas::ReaderFactory f;
 	liblas::Reader reader = f.CreateWithStream(ifs);
@@ -126,21 +123,23 @@ int main(int argc, char* argv[]) {
 	std::cout << "Points count: " << header.GetPointRecordsCount() << '\n';
     
 	//~ The voronoi diagram
-    voronoi_diagram<double> vd;
-    
+	voronoi_diagram<double> vd;
+
 	while (reader.ReadNextPoint())
 	{
 		liblas::Point const& p = reader.GetPoint();
-        //~ points.push_back(point_data<double>(p.GetX(),p.GetY()));
-        points.push_back(OurPoint(p.GetRawX(),p.GetRawY(),p.GetRawZ(),p.GetReturnNumber()));
-        //~ boost::polygon::insert<OurPoint,voronoi_diagram<double> >(p, &vd);
+		//~ points.push_back(point_data<double>(p.GetX(),p.GetY()));
+		points.push_back(OurPoint(p.GetRawX(),p.GetRawY(),p.GetRawZ(),p.GetReturnNumber()));
+		//~ boost::polygon::insert<OurPoint,voronoi_diagram<double> >(p, &vd);
 
 		//~ std::cout << p.GetX() << ", " << p.GetY() << ", " << p.GetZ() << "\n";
 	}
-    construct_voronoi(points.begin(), points.end(), &vd);
-    
-    std::cout << "Num of cells: " << vd.num_cells() << std::endl;
-    std::cout << "Num of edges: " << vd.num_edges() << std::endl;
+	construct_voronoi(points.begin(), points.end(), &vd);
+
+	std::cout << "Num of cells: " << vd.num_cells() << std::endl;
+	std::cout << "Num of edges: " << vd.num_edges() << std::endl;
     
 	iterateCells(vd, points);
+	return 0;
 }
+
