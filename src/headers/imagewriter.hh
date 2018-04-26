4553b718ea0256d01c8e40e16b7ffe82fb47e485
Szabolcs <topika@gmail.com>
2018-04-26 13:14:17 +0200

    OpenCV adjusted to current state

--- src/headers/imagewriter.hh
+++ src/headers/imagewriter.hh
@@ -1,19 +1,24 @@
 #ifndef __IMAGEWRITER_HH
 #define __IMAGEWRITER_HH
 
-#define maxX 1280
-#define maxY 720
+#include <vector>
+#include <iostream>
+
+//~ #define maxX 1280
+//~ #define maxY 720
 
 class ImageWriter {
     public:
     ImageWriter(double bottomleftX_, double bottomleftY_, double toprightX_, double toprightY_) : bottomleftX(bottomleftX_), bottomleftY(bottomleftY_), toprightX(toprightX_), toprightY(toprightY_) {
-        for(int i = 0;i < maxX;++i) {
-            for(int j = 0;j<maxY;++j) {
-                for(int k = 0;k<4;++k) {
-                    numbers[i][j][k] = 0;
-                }
-            }
-        }
+        numbers = std::vector<std::vector<std::vector<int> > >(maxX, std::vector<std::vector<int> >(maxY, std::vector<int>(4,0)));
+        std::cout << "Constr. done" <<std::endl;
+        //~ for(int i = 0;i < maxX;++i) {
+            //~ for(int j = 0;j<maxY;++j) {
+                //~ for(int k = 0;k<4;++k) {
+                    //~ numbers[i][j][k] = 0;
+                //~ }
+            //~ }
+        //~ }
      }
 
     void addPoint(const OurPoint& newPoint);
@@ -22,13 +27,16 @@ class ImageWriter {
     private:
     int bottomleftX, bottomleftY;
     int toprightX, toprightY;
-    short numbers[maxX][maxY][4];
+    const int maxX = 1280;
+    const int maxY = 720;
+    //~ int numbers[maxX][maxY][4];
+    std::vector<std::vector<std::vector<int> > > numbers;
     
     inline int getScaled(int val, bool x_) {
         if (x_)
-            return (int)((val - bottomleftX) * maxX / (toprightX-bottomleftX));
+            return (int)((val - bottomleftX) * (maxX-1) / (toprightX-bottomleftX));
         else
-            return (int)((val - bottomleftY) * maxY / (toprightY-bottomleftY));
+            return (int)((val - bottomleftY) * (maxY-1) / (toprightY-bottomleftY));
     }
 };
 
--- src/writepic.cc
+++ src/writepic.cc
@@ -10,37 +10,46 @@ using namespace std;
 
 static Vec3b toColor(int x) {
     switch(x) {
-        case 0: return Vec3b(255,0,0);
-        case 1: return Vec3b(0,255,0);
-        case 2: return Vec3b(0,0,255);
-        case 3: return Vec3b(128,128,255);
-        default: return Vec3b(0,0,0);
+        case 0: return Vec3b(42,42,165); //talaj, barna
+        case 1:
+            return Vec3b(0,255,0); // növény, zöld
+        case 2:
+            return Vec3b(0,0,255); // épület, vörös
+        case 3:
+            return Vec3b(32,32,32); // út, fekete
+        default:
+            return Vec3b(255,255,255);
+        
+        //~ case 0: return Vec3b(255,0,0);
+        //~ case 1: return Vec3b(0,255,0);
+        //~ case 2: return Vec3b(0,0,255);
+        //~ case 3: return Vec3b(128,128,255);
+        //~ default: return Vec3b(0,0,0);
     }
     
 }
-//~ class ImageWriter {
-    //~ public:
-    //~ ImageWriter() {}
-    //~ void init(int xSize,int ySize, int t);
-    //~ void addPoint(const OurPoint& newPoint);
-    //~ void writeToFile(const std::string& fileName);
-    //~ 
-    //~ private:
-    //~ Mat mat;
-//~ } 
 
 void ImageWriter::addPoint(const OurPoint& newPoint) {
     int32_t x = newPoint.getX();
     int32_t y = newPoint.getY();
     preProcClass realClass = newPoint.getPreClass();
         //~ cout << (int)(x * scaleX) << " " <<(int)( y * scaleY )<< endl;
-
-    ++numbers[getScaled(x,true)][getScaled(y,false)][realClass];
+    int transformed = -1;
+    if(realClass == uniformSurface) transformed = 0;
+    else if(realClass == nonUniformSurface || realClass == lowerContour || realClass == upperContour) transformed = 1;
+    else if(realClass == building) transformed = 2;
+    else if(realClass == road) transformed = 3;
+    //~ cout << getScaled(x,true) << " " << getScaled(y,false) << " " << transformed << endl;
+    if(transformed != -1 && transformed != 2)
+        ++numbers[getScaled(x,true)][getScaled(y,false)][transformed];
+    else if (transformed == 2)
+        numbers[getScaled(x,true)][getScaled(y,false)][transformed] += 1;
+    //~ cout << "end" << endl;
     //~ short numbers[1280][720][4];
 }
 
 void ImageWriter::writeToFile(const std::string& fileName) {
-    Mat mat(maxX,maxY,CV_8UC3);
+    Mat mat(maxY,maxX,CV_8UC3);
     for(int i = 0; i < maxX; ++i) {
         for(int j = 0; j < maxY; ++j) {
             int max = numbers[i][j][0], maxid = 0;
